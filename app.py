14da010f9c110054c319db2744d1b1afe525311c
Faud X <77972137+Fuad-X@users.noreply.github.com>
2024-01-30 21:13:11 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -71,5 +71,5 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=False,host="0.0.0.0")
+    app.run(debug=True)
 
